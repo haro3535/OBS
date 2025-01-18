05408008aac8c54b3ff00e3ac51f6d79d64b1cb3
Harun Onur <66617253+haro3535@users.noreply.github.com>
2025-01-19 01:04:06 +0300

    Update on dashboard
    Some Dashbox samples

--- nextapp/src/app/dashboard/page.tsx
+++ nextapp/src/app/dashboard/page.tsx
@@ -7,6 +7,26 @@ export default function Page() {
         <div className="space-y-6">
             <Dashbox title="Takvim" children={CalenderModule()}/>
             <Dashbox title="Duyurular" children={"Duyuruları buradan yapabilirsiniz!"}/>
+            <div className="grid grid-cols-2 gap-6">
+                <Dashbox title="Bölüm 1" children={"Buraya ne geleceği sonrasında belirlenebilir"}/>
+                <Dashbox title="Bölüm 2" children={"Buraya ne geleceği sonrasında belirlenebilir"}/>
+            </div>
+            <div className="grid grid-cols-3 gap-6">
+                <Dashbox title="Bölüm 3" children={"Buraya ne geleceği sonrasında belirlenebilir"}/>
+                <Dashbox title="Bölüm 4" children={"Buraya ne geleceği sonrasında belirlenebilir"}/>
+                <Dashbox title="Bölüm 5" children={"Buraya ne geleceği sonrasında belirlenebilir"}/>
+            </div>
+            <div className="grid grid-cols-2 gap-6">
+                <Dashbox title="Bölüm 6" children={
+                    <div className="h-40">
+                        "Buraya ne geleceği sonrasında belirlenebilir"
+                    </div>
+                }/>
+                <div className="grid grid-cols-1 gap-6">
+                    <Dashbox title="Bölüm 7" children={"Buraya ne geleceği sonrasında belirlenebilir"}/>
+                    <Dashbox title="Bölüm 8" children={"Buraya ne geleceği sonrasında belirlenebilir"}/>
+                </div>
+            </div>
         </div>
     );
 }
--- nextapp/src/app/ui/dashboard/sidenav.tsx
+++ nextapp/src/app/ui/dashboard/sidenav.tsx
@@ -1,31 +1,55 @@
 import Link from "next/link";
 
 
+// const SideBarElement = ({title, children}) => {
+//     return(
+//         <div>
+
+//         </div>
+//     );
+// }
+
+
 export default function Sidenav() {
     return (
         <nav className="flex flex-col w-64 h-full bg-gray-800">
             <div className="flex items-center justify-center h-16 bg-gray-900">
-                <h1 className="text-white">Dashboard</h1>
+                <h1 className="text-white">İzmir Ekonomi Üniversitesi</h1>
             </div>
             <ul className="flex flex-col items-start w-full">
                 <li className="w-full">
                     <a href="#" className="flex items-center w-full h-12 px-6 hover:bg-gray-700">
-                        <span className="text-white">Home</span>
+                        <span className="text-white">Genel İşlemler</span>
                     </a>
                 </li>
                 <li className="w-full">
                     <a href="#" className="flex items-center w-full h-12 px-6 hover:bg-gray-700">
-                        <span className="text-white">Users</span>
+                        <span className="text-white">Ders ve Dönem İşlemleri</span>
                     </a>
                 </li>
                 <li className="w-full">
                     <a href="#" className="flex items-center w-full h-12 px-6 hover:bg-gray-700">
-                        <span className="text-white">Settings</span>
+                        <span className="text-white">Form İşlemleri</span>
                     </a>
+                </li>
+                <li className="w-full">
+                    <Link href="/dashboard/#" className="flex items-center w-full h-12 px-6 hover:bg-gray-700">
+                        <span className="text-white">YÖKSİS İşlemleri</span>
+                    </Link>
+                </li>
+                <li className="w-full">
+                    <Link href="/dashboard/#" className="flex items-center w-full h-12 px-6 hover:bg-gray-700">
+                        <span className="text-white">Başvuru İşlemleri</span>
+                    </Link>
+                </li>
+                <li className="w-full">
+                    <Link href="/dashboard/#" className="flex items-center w-full h-12 px-6 hover:bg-gray-700">
+                        <span className="text-white">Kullanıcı İşlemleri</span>
+                    </Link>
                 </li>
                 <li className="w-full">
                     <Link href="/dashboard/messages" className="flex items-center w-full h-12 px-6 hover:bg-gray-700">
-                        <span className="text-white">Messages</span>
+                        <span className="text-white">Mesajlar</span>
                     </Link>
                 </li>
             </ul>
