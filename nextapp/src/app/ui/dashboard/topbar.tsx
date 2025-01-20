b159f4e47a9a5944dc9d8b176c4541866cc09a81
Harun Onur <66617253+haro3535@users.noreply.github.com>
2025-01-20 23:07:25 +0300

    Notification updated

--- nextapp/src/app/ui/dashboard/topbar.tsx
+++ nextapp/src/app/ui/dashboard/topbar.tsx
@@ -1,25 +1,66 @@
+"use client";
 import Image from 'next/image';
-
+import Link from 'next/link';
+import { Dropdown } from 'flowbite-react';
 
 
 export default function Topbar() {
     return (
         <nav className="w-full flex items-center justify-end bg-[--barground] h-16 shadow-sm">
             <div className="py-2 px-4 flex items-center space-x-4">
-                <div className='flex cursor-pointer'>
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        height="24px" 
-                        viewBox="0 -960 960 960" 
-                        width="24px" 
-                        className=' svg-color '
-                    >
-                        <path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z"/>
-                    </svg>
-                    <span className="relative flex h-3 w-3 z-10 ml-[-0.5rem]">
-                            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                            <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-                    </span>
+                <div>
+                    <Dropdown label="" className='mt-5 ml-[-1.5vw] bg-barground border-gray-900' dismissOnClick={false} renderTrigger={() => 
+                        <button type='button' title='notifications' id="dropdownNotificationButton" className="relative mr-3 inline-flex items-center text-sm font-medium text-center text-gray-500 hover:text-gray-900 focus:outline-none dark:hover:text-white dark:text-gray-400">
+                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
+                                <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z"/>
+                            </svg>
+                            <div className="absolute block w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-0.5 start-2.5 dark:border-gray-900"></div>
+                        </button>
+                        }>
+                        <Dropdown.Header className='text-center text-textcolor'>
+                            Notification
+                        </Dropdown.Header>
+                        <Dropdown.Item as={Link} href="/dashboard/messages" className='bg-barground hover:bg-bargroundhover'>
+                            <div className="flex-shrink-0">
+                                <Image className="rounded-full w-11 h-11" src="/pp.jpg" width={50} height={50} alt="Jese image" />
+                                <div className="absolute flex items-center justify-center w-5 h-5 ms-6 -mt-5 bg-blue-600 border border-white rounded-full dark:border-gray-800">
+                                <svg className="w-2 h-2 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                    <path d="M1 18h16a1 1 0 0 0 1-1v-6h-4.439a.99.99 0 0 0-.908.6 3.978 3.978 0 0 1-7.306 0 .99.99 0 0 0-.908-.6H0v6a1 1 0 0 0 1 1Z"/>
+                                    <path d="M4.439 9a2.99 2.99 0 0 1 2.742 1.8 1.977 1.977 0 0 0 3.638 0A2.99 2.99 0 0 1 13.561 9H17.8L15.977.783A1 1 0 0 0 15 0H3a1 1 0 0 0-.977.783L.2 9h4.239Z"/>
+                                </svg>
+                                </div>
+                            </div>
+                            <div className="w-full ps-3">
+                                <div className="text-textcolor max-w-80 text-sm mb-1.5">New message from <span className="font-semibold text-textcolor">Jese Leos</span>: "Hey, what's up? All set for the presentation?"</div>
+                                <div className="text-xs text-blue-600 dark:text-blue-500">a few moments ago</div>
+                            </div>
+                        </Dropdown.Item>
+                        <Dropdown.Divider className='bg-gray-600 my-0'/>
+                        <Dropdown.Item as={Link} href="/dashboard/messages" className='bg-barground hover:bg-bargroundhover'>
+                            <div className="flex-shrink-0">
+                                <Image className="rounded-full w-11 h-11" src="/pp.jpg" width={50} height={50} alt="Jese image" />
+                                <div className="absolute flex items-center justify-center w-5 h-5 ms-6 -mt-5 bg-blue-600 border border-white rounded-full dark:border-gray-800">
+                                <svg className="w-2 h-2 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                    <path d="M1 18h16a1 1 0 0 0 1-1v-6h-4.439a.99.99 0 0 0-.908.6 3.978 3.978 0 0 1-7.306 0 .99.99 0 0 0-.908-.6H0v6a1 1 0 0 0 1 1Z"/>
+                                    <path d="M4.439 9a2.99 2.99 0 0 1 2.742 1.8 1.977 1.977 0 0 0 3.638 0A2.99 2.99 0 0 1 13.561 9H17.8L15.977.783A1 1 0 0 0 15 0H3a1 1 0 0 0-.977.783L.2 9h4.239Z"/>
+                                </svg>
+                                </div>
+                            </div>
+                            <div className="w-full ps-3">
+                                <div className="text-textcolor max-w-80 text-sm mb-1.5">New message from <span className="font-semibold text-textcolor">Jese Leos</span>: "Hey, what's up? All set for the presentation?"</div>
+                                <div className="text-xs text-blue-600 dark:text-blue-500">a few moments ago</div>
+                            </div>
+                        </Dropdown.Item>
+                        <Dropdown.Divider className='bg-gray-600 my-0'/>
+                        <Dropdown.Item className='bg-barground hover:bg-bargroundhover flex items-center justify-center text-textcolor'>
+                            <div className="inline-flex items-center ">
+                                <svg className="w-4 h-4 me-2 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
+                                    <path d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
+                                </svg>
+                                View all
+                            </div>
+                        </Dropdown.Item>
+                    </Dropdown>
                 </div>
                 <div className='flex items-center justify-center w-12 h-12 bg-[--barground] rounded-full shadow-md'>
                     <Image src="/pp.jpg" className='rounded-full' alt="Profile picture" width={50} height={50} quality={100}/>
--- nextapp/tailwind.config.ts
+++ nextapp/tailwind.config.ts
@@ -1,18 +1,25 @@
 import type { Config } from "tailwindcss";
+const flowbite = require("flowbite-react/tailwind");
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        barground: "var(--barground)",
+        textcolor: "var(--textcolor)",
+        bargroundhover: "var(--barground-hover)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 } satisfies Config;
